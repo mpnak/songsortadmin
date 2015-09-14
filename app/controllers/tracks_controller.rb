b21c1ce44f97a223d797993b5c6996931f0fd84b
Vaughan Kelly Guy <vkellyguy@gmail.com>
2015-09-14 14:08:37 -0400

    passing track controller tests. Disabled authentication for now

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -3,13 +3,13 @@ class TracksController < ApplicationController
   def create
     @station = Station.find(track_params[:station_id])
 
-    track = Track.build_from_spotify_id(track_params[:spotify_id])
-    track.station = @station
+    @track = Track.build_from_spotify_id(track_params[:spotify_id])
+    @track.station = @station
 
-    if track.save
-      render json: track, status: 201, location: track
+    if @track.save
+      render json: @track, status: 201, location: @track
     else
-      render json: { errors: track.errors }, status: 422
+      render json: { errors: @track.errors }, status: 422
     end
   end
 
--- spec/controllers/tracks_controller_spec.rb
+++ spec/controllers/tracks_controller_spec.rb
@@ -12,28 +12,30 @@ RSpec.describe TracksController, type: :controller do
     skip("Add a hash of attributes invalid for your model")
   }
 
-  let(:spotify_attributes) do
-    {
-      spotify_id: "0ng5s1sqaQ3K0NhMDN7WSL",
-      station_id: 1
-    }
-  end
+  let(:spotify_id) { "0ng5s1sqaQ3K0NhMDN7WSL" }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # TracksController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  let(:valid_session) do
+    {}
+  end
 
   describe "POST #create" do
+
+    before(:each) do
+      @station = FactoryGirl.create(:station)
+    end
+
     context "with valid params" do
       it "creates a new Track" do
         expect {
-          post :create, {:track => spotify_attributes}, valid_session
+          post :create, {:track => {spotify_id: spotify_id, station_id: @station.id} }, valid_session
         }.to change(Track, :count).by(1)
       end
 
       it "assigns a newly created track as @track" do
-        post :create, {:track => spotify_attributes}, valid_session
+        post :create, {:track => {spotify_id: spotify_id, station_id: @station.id} }, valid_session
         expect(assigns(:track)).to be_a(Track)
         expect(assigns(:track)).to be_persisted
       end
