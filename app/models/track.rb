36480a6ad87ab094a1b618baf5cb02ef72c45deb
Vaughan Kelly Guy <vkellyguy@gmail.com>
2015-09-17 17:45:52 -0400

    update undergroundness in taste profile

--- app/models/track.rb
+++ app/models/track.rb
@@ -5,6 +5,7 @@ class Track < ActiveRecord::Base
 
   before_create :create_in_taste_profile
   before_destroy :destroy_from_taste_profile
+  before_update :update_taste_profile
 
   def self.build_from_spotify_id(spotify_id)
     track = Echowrap.track_profile(:id => "spotify:track:#{spotify_id}")
@@ -35,7 +36,30 @@ class Track < ActiveRecord::Base
     data = [
       {
         action: "delete",
-        item: { item_id: self.echo_nest_id }
+        item: { track_id: self.echo_nest_id }
+      }
+    ]
+
+    Echowrap.taste_profile_update(
+      :id => self.station.taste_profile_id,
+      :data => JSON.generate(data)
+    )
+  end
+
+  # Here we are only concerned with setting the undergroundness on the taste
+  # profile
+  def update_taste_profile
+    return unless self.undergroundness
+
+    data = [
+      {
+        action: "update",
+        item: {
+          track_id: self.echo_nest_id,
+          item_keyvalues: {
+            undergoundness: self.undergroundness
+          }
+        }
       }
     ]
 
