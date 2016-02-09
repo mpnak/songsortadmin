706680e47f160159c2aa1b2462cd0f5bfe29e4b2
Vaughan Kelly Guy <vkellyguy@gmail.com>
2016-02-09 16:26:29 -0500

    basic authentication for html pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #http_basic_authenticate_with name: "user", password: "password" unless Rails.env.test?
+  before_filter :authenticate
+
+  def authenticate
+    unless request.format.json?
+      authenticate_or_request_with_http_basic('Administration') do |username, password|
+        username == 'doseadmin' && password == 'bellyflop'
+      end
+    end
+  end
 
 end
